05ef227f89d4ebadc4e97793c0646f1f881f05d3
Karolyne Goncalves <karolyne.goncalves@esags.edu.br>
2026-08-08 17:18:49 -0300

    Liga painel na planilha ao vivo (novo Apps Script /exec)

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,6 +1,6 @@
 // Preencha APPS_SCRIPT_URL depois de implantar o Apps Script (Implantar > Nova implantação > App da Web > copie a URL /exec).
 // GOOGLE_CLIENT_ID já está preenchido com o mesmo do painel Leve Sonho (mesma origem github.io, então funciona).
 window.PAINEL_CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzxIe7uyQ_Px7oMs4eXBgz5BFx4u4aFF6uDd2xmcqwuZrvpglhJRIAAYCVfs-8cuoU/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxog03tL0hWW7mNwk9mfjvsQfNZv0jOHhLU7tUqcjmGlBJ2qhBDJiy_CYBDPGnFQ4q2/exec',
   GOOGLE_CLIENT_ID: '220743716320-ia319tnsqqj0mlr7hre4bi0m6qi658vm.apps.googleusercontent.com'
 };
