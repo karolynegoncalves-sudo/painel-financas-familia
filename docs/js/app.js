116222c0888578a5e6c8cde3eadb96e548bbf419
Karolyne Goncalves <karolyne.goncalves@esags.edu.br>
2026-08-08 18:28:26 -0300

    Card de reserva ao vivo

--- docs/js/app.js
+++ docs/js/app.js
@@ -51,6 +51,7 @@ function renderVisao(){
    <div class="kpi sal"><div class="l">Sobra</div><div class="v serif">${BRL(k.sob)}</div><div class="h">pra investir</div></div>
    <div class="kpi wt"><div class="l">Taxa de poupança</div><div class="v serif ${k.taxa>=20?'pos':'neg'}">${k.taxa.toFixed(0)}%</div><div class="h">meta 20%</div></div>
    <div class="kpi wt"><div class="l">Reserva</div><div class="v serif pos">${BRL(D.kpi.reserva)}</div><div class="h">da aba Reservas</div></div>`;
+  if(el("resVal"))el("resVal").textContent=BRL(D.kpi.reserva);
   drawCombo();drawDonut(catAtual());drawGauge(k.taxa);
 }
 function drawCombo(){
